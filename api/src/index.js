60ed287883aa86d8d521175785d35ed2bc6e75f2
Reza Rahman <rezrah@github.com>
2022-08-08 19:30:42 +0100

    initial commit

---
+++ api/src/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+
+const { generateMainImage } = require("./generate-image");
+
+// defining the Express app
+const app = express();
+
+// // defining an array to work as the database (temporary solution)
+// const ads = [{ title: "Hello, world (again)!" }];
+
+app.use(express.static(path.resolve(__dirname, "../views/images")));
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan("combined"));
+
+// defining an endpoint to return all ads
+// app.get("/", (req, res) => {
+//   res.send(ads);
+// });
+
+app.post("/", async (req, res) => {
+  const { title, category } = req.body;
+  const success = await generateMainImage(
+    "test",
+    ["#161B22", "#161B22"],
+    category,
+    title,
+    undefined,
+    true
+  );
+  console.log(success);
+  res.send({ ...success });
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
