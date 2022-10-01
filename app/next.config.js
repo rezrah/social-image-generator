20c25b3b19bc36d3faa744117c4cb7da5fa58c4c
Reza Rahman <rezrah@github.com>
2022-10-01 23:17:02 +0100

    add base path

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath:
+    process.env.NODE_ENV === "production" ? "/social-image-generator" : "",
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
