089b5039f8e732b1cd1c6a27c4548b5e6681474e
Reza Rahman <rezrah@github.com>
2022-12-27 18:54:17 +0000

    add favicons

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+      </Head>
       <body data-color-mode="dark">
         <Main />
         <NextScript />
